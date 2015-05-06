55938340cc82c0b5a299f186a83904dad0308eb8
ShowcaseEngine <9kzezi@gmail.com>
2015-05-06 10:33:13 -0500

    Create Four.php
    Initial commit

---
+++ Four.php
@@ -0,0 +1,39 @@
+<?php namespace Bundle\Framework;
+
+class Four {
+	public static function value() {
+		return 4;
+	}
+
+	public static function add() {
+		return 4 + 4;
+	}
+
+	public static function subtract() {
+		return 4-4;
+	}
+
+	public static function multiply() {
+		return 4 * 4;
+	}
+
+	public static function divide() {
+		return 4/4;
+	}
+
+	public static function sqrt() {
+		return 2;
+	}
+
+	public static function english() {
+		return "four";
+	}
+
+	public static function spanish() {
+		return "cuatro";
+	}
+
+	public static function french() {
+		return "quatre";
+	}
+}
